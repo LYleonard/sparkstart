31b9294c62ca13628e95db8dbc5b5c0cc33bcee8
LYleonard <aygxywrp@163.com>
2019-09-17 14:15:37 +0800

    Broadcast Variable

--- src/main/scala/cn/wrp/BroadcastVariable.scala
+++ src/main/scala/cn/wrp/BroadcastVariable.scala
@@ -6,6 +6,8 @@ import org.apache.spark.{SparkConf, SparkContext}
   * * @Description 共享变量
   * * @Author LYleonard
   * * @Date 2019/9/17 11:51
+  * * 广播变量将一个只读变量发送到每台机器上，不用在任务之间传递变量。
+  * * 保证数据一致性，spark只支持broadcast只读变量。
   **/
 object BroadcastVariable {
   def main(args: Array[String]): Unit = {
