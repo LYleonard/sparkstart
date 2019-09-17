60bcad0f4b81c7a3c331fd22c6bbb9bedb3ff5f2
LYleonard <aygxywrp@163.com>
2019-09-17 17:46:31 +0800

    Sort WordCounts by Scala

---
+++ src/main/scala/cn/wrp/WordCountSorted.scala
@@ -0,0 +1,28 @@
+package cn.wrp
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * * @Description 根据出现次数降序排列WordCount
+  * * @Author LYleonard
+  * * @Date 2019/9/17 17:25
+  **/
+object WordCountSorted {
+  def main(args: Array[String]): Unit = {
+    wordCountSorted()
+  }
+
+  def wordCountSorted(): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("WordCountSorted").setMaster("local")
+    val sc: SparkContext = new SparkContext(conf)
+
+    val lines = sc.textFile("file:///E:\\develop\\Spark\\sparkstart\\src\\test\\scala\\cn\\wrp\\wordcount.txt")
+    val words = lines.flatMap(x => x.split(" "))
+    val wordcounts = words.map(word => (word, 1)).reduceByKey(_ + _)
+
+    val result = wordcounts.map(count => (count._2, count._1)).sortByKey(false)
+//      .map(x => (x._2, x._1))
+//    result.foreach(x => println(x._1 + ": " + x._2))
+    result.foreach(x => println(x._2 + ": " + x._1))
+  }
+}
