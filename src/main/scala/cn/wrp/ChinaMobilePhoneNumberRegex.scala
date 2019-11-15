7ea0d168ebf5c60366609801af55230b3fd64cd3
LYleonard <aygxywrp@163.com>
2019-11-15 17:02:55 +0800

    China Mobile PhoneNumbers Regex

---
+++ src/main/scala/cn/wrp/ChinaMobilePhoneNumberRegex.scala
@@ -0,0 +1,31 @@
+package cn.wrp
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @Author LYleonard
+  * @Date 2019/11/15 16:07
+  * @Description scala正则表达式过滤中国手机号
+  */
+object ChinaMobilePhoneNumberRegex {
+  def main(args: Array[String]): Unit = {
+    val sparkSession: SparkSession = SparkSession.builder().
+      appName("ChinaMobilePhoneNumberFilter").master("local[*]").getOrCreate()
+
+    val phonesDF = sparkSession.read.option("header", "true").csv("E:\\develop\\Spark\\sparkstart\\src\\test\\scala\\cn\\wrp\\phones.csv")
+    val purePhonesDF = phonesDF.filter(x=>{
+      println("X: ", x)
+      var flag=false
+      val phoneNum=x.getAs[String]("phones")
+      val pattern = """^(?:\+?86)?1(?:3\d{3}|5[^4\D]\d{2}|8\d{3}|7(?:[01356789]\d{2}|4(?:0\d|1[0-2]|9\d))|9[189]\d{2}|6[567]\d{2}|4(?:[14]0\d{3}|[68]\d{4}|[579]\d{2}))\d{6}$""".r
+
+      println(pattern findFirstMatchIn phoneNum)
+      println("----------------------")
+
+      if ((pattern findFirstMatchIn phoneNum) != None){ flag = true }
+      flag
+    })
+
+    purePhonesDF.show()
+  }
+}
