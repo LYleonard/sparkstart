2322104d588ecaf6e156b3485ed4774b4bfd203d
LYleonard <aygxywrp@163.com>
2020-01-07 15:31:28 +0800

    修改配置

--- src/main/scala/cn/wrp/ReadFromFTP.scala
+++ src/main/scala/cn/wrp/ReadFromFTP.scala
@@ -1,6 +1,7 @@
 package cn.wrp
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 /**
   * @Description 读写ftp服务器
@@ -11,15 +12,23 @@ import org.apache.spark.sql.SparkSession
 object ReadFromFTP {
   def main(args: Array[String]): Unit = {
     val sparkSession = SparkSession.builder()
-      .master("local[*]")
+      .master("local")
       .appName("ReadDataFromFTP").getOrCreate()
+
+    val dataSchema = StructType(List(
+      StructField("C1", StringType),
+      StructField("C2", StringType),
+      StructField("C3", StringType),
+      StructField("C4", StringType)
+    ))
 
     val df = sparkSession.read.
       format("com.springml.spark.sftp").
       option("host", "cp-nifi-cluster-01-lkj").//主机名
       option("username", "root").//注意这个的用户名为系统用户名或者具有指定路径访问权限的用户名
       option("port", "22").
-      option("header", "false").
+      option("header", "true").
+      schema(dataSchema).
       option("password", "cetc@2017").
       option("fileType", "csv").
       option("delimiter", "|").
@@ -31,6 +40,7 @@ object ReadFromFTP {
       load("/root/testftp/test.csv") //系统目录，不是ftp的访问的根目录
 
     df.show()
+    df.printSchema()
 
   }
 }
