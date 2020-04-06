86452b76b14fd5d2daa327543a4282859fce6326
LYleonard <aygxywrp@163.com>
2020-04-07 00:42:00 +0800

    spark df to Orc partition table

---
+++ src/main/scala/cn/wrp/Spark2OrcPartition.scala
@@ -0,0 +1,25 @@
+package cn.wrp
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
+
+/**
+  * @Author LYleonard
+  * @Date 2020/3/31 23:52
+  * @Description TODO
+  */
+object Spark2OrcPartition {
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder().appName("TestPhoneGeo")
+//      .master("local")   //
+      .enableHiveSupport()            // spark读写hive表配置
+      .config("hive.exec.dynamic.partition.mode", "nonstrict")
+      .getOrCreate()
+
+    import sparkSession.implicits._
+    val DF = sparkSession.read.option("header","true").csv("hdfs://192.168.29.129:9000/test/phones.csv")
+
+    // test.phone_orc表以level列字段分区
+    DF.withColumn("level", lit(1)).write.mode(SaveMode.Append).format("orc").insertInto("test.phone_orc")
+  }
+}
